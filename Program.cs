882962a4342fe0c9d7b2f033f0bc9a3975d42703
Oliwia <oliwia.czap13@gmail.com>
2026-02-04 10:59:03 +0100

    działający kod

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿class Klasa
+﻿using System;
+class Klasa
 {
-    public void Main()
+    static void Main()
     {
         Console.WriteLine("test praktyczny 2025 czerwiec zad 2");
 
@@ -10,5 +11,35 @@
         Console.WriteLine("Podaj słowo do zaszyfrowania: ");
         string slowo = Console.ReadLine();
 
+        string wynik = Szyfruj(przesuniecie, slowo);
+
+        Console.WriteLine("\nZaszyfrowany tekst: ");
+        Console.WriteLine(wynik);
+
+        Console.ReadKey();
+    }
+
+    static string Szyfruj(int przesuniecie, string slowo)
+    {
+        przesuniecie = przesuniecie % 26;
+        char[] wynik = new char[slowo.Length];
+
+        for (int i = 0 ; i < slowo.Length; i++)
+        {
+            char znak = slowo[i];
+
+            if (znak == ' ')
+            {
+                wynik[i] = ' ';
+                continue;
+            }
+
+            int pozycja = znak - 'a';
+            int nowaPozycja = (pozycja + przesuniecie + 26) % 26;
+
+            wynik[i] = (char)('a' + nowaPozycja);
+        }
+
+        return new string(wynik);
     }
 }
