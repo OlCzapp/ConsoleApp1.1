3de912c5ceb246ad57edc23f03048c2460249b0d
Oliwia <oliwia.czap13@gmail.com>
2026-02-04 10:40:55 +0100

    Dwa pytania

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿class Klasa
 {
-    public void Metoda()
+    public void Main()
     {
-        System.Console.WriteLine("test praktyczny 2025 czerwiec zad 2");
+        Console.WriteLine("test praktyczny 2025 czerwiec zad 2");
+
+        Console.WriteLine("Podaj cyfrę, o ile miejsc słowo ma być zaszyfrowane: ");
+        int przesuniecie = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj słowo do zaszyfrowania: ");
+        string slowo = Console.ReadLine();
+
     }
 }
